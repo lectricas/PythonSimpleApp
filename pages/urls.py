15fd912016aa8193470944239de145acf968132f
lectricas <lectricas@gmail.com>
2022-09-20 12:21:07 +0300

    added requests

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import homePageView
+from .views import pathView
+from .views import queryView
+from .views import bodyView
 
 urlpatterns = [
     path("", homePageView, name="home"),
+    path('path/<str:count>/', pathView, name = "path"),
+    path('query/', queryView, name = "query"),
+    path('body/', bodyView, name = "body"), #зачем нам нужен name везде?
 ]
