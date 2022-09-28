5fba0077eaa255e1de699a3c2d25e0947617f815
lectricas <lectricas@gmail.com>
2022-09-28 13:06:22 +0300

    added a few tests

--- bank/tests.py
+++ bank/tests.py
@@ -1,15 +1,88 @@
 from django.test import TestCase
 from django.urls import reverse
-
 from django.test import Client
 
+from bank.models import Account
 
-class AccountTests(TestCase):
 
-    def test_create_account(self):
+class AccountBasicTests(TestCase):
+
+    def test_create_account_ok(self):
+        c = Client()
+        response = c.post(
+            reverse('bank:create_account'),
+            '{"currency": "RUB", "name": "qwer"}',
+            content_type="application/json"
+        )
+        self.assertContains(response, 'Ok')
+
+    def test_create_account_error(self):
+        account = Account(pk=1, currency="RUB", name="1")
+        account.save()
+        c = Client()
+        response = c.post(
+            reverse('bank:create_account'),
+            '{"currency": "RUB", "name": "1"}',
+            content_type="application/json"
+        )
+        self.assertContains(response, 'Error')
+
+    def test_delete_account_ok(self):
+        account = Account(pk=1, currency="RUB", name="1")
+        account.save()
+        c = Client()
+        response = c.post(
+            reverse('bank:delete_account'),
+            '{"account_id": "1"}',
+            content_type="application/json"
+        )
+        self.assertContains(response, 'Ok')
+
+    def test_delete_account_error(self):
+        c = Client()
+        response = c.post(
+            reverse('bank:delete_account'),
+            '{"account_id": "1"}',
+            content_type="application/json"
+        )
+        self.assertContains(response, 'Error')
+
+
+class AccountIntegrationTests(TestCase):
+
+    def test_create_delete_account(self):
         c = Client()
         c.post(
             reverse('bank:create_account'),
             '{"currency": "RUB", "name": "qwer"}',
             content_type="application/json"
         )
+
+        response = c.post(
+            reverse('bank:delete_account'),
+            '{"account_id": "1"}',
+            content_type="application/json"
+        )
+        self.assertContains(response, 'Ok')
+
+    def test_create_delete_delete_account(self):
+        c = Client()
+        c.post(
+            reverse('bank:create_account'),
+            '{"currency": "RUB", "name": "qwer"}',
+            content_type="application/json"
+        )
+
+        c.post(
+            reverse('bank:delete_account'),
+            '{"account_id": "1"}',
+            content_type="application/json"
+        )
+
+        response = c.post(
+            reverse('bank:delete_account'),
+            '{"account_id": "1"}',
+            content_type="application/json"
+        )
+
+        self.assertContains(response, 'Error')
--- bank/views.py
+++ bank/views.py
@@ -1,30 +1,8 @@
-from django.http import HttpResponse
-from django.conf import settings
 import json
+
+from django.http import HttpResponse
+
 from bank.models import Account
-from django.shortcuts import render, redirect, get_object_or_404
-from django.core import serializers
-
-
-# Create your views here.
-
-def deposit(request):
-    # db = settings.DATABASE
-    # accounts = db.accounts
-    # amount = request.GET.get('amount', None)
-    # currency = request.GET.get('currency', None)
-    # account_number = request.GET.get('account_number', None)
-
-    # deposit = Deposit(account_number, currency, amount)
-
-    return HttpResponse("Deposit " + " hello")
-
-
-def withdraw(request):
-    # amount = request.GET.get('amount', None)
-    # currency = request.GET.get('currency', None)
-    # account_number = request.GET.get('account_number', None)
-    return HttpResponse("Withdraw " + "hello")
 
 
 def create_account(request):
@@ -34,12 +12,12 @@ def create_account(request):
         currency = body['currency']
         name = body['name']
         try:
-            _ = Account.objects.get(name=name)  # так?
-            return HttpResponse("Exists")
+            _ = Account.objects.get(name=name)
+            return HttpResponse('{"result":"Error"}', content_type="application/json")
         except Exception as e:
             obj = Account.objects.create(name=name, currency=currency)
             obj.save()
-            return HttpResponse("Ok")
+            return HttpResponse('{"result":"Ok"}', content_type="application/json")
 
 
 def delete_account(request):
@@ -48,12 +26,10 @@ def delete_account(request):
         body = json.loads(body_unicode)
         account_id = body['account_id']
         try:
-            count, _ = Account.objects.get(pk=account_id).delete()  # или так?
+            count, _ = Account.objects.get(pk=account_id).delete()
             if count == 1:
-                return render(HttpResponse("Deleted"))
+                return HttpResponse("Ok")
             else:
-                return render(HttpResponse("404"))
+                return HttpResponse("404")
         except Exception as e:
-            return render(HttpResponse("Does not exists"))
-
-    return render(HttpResponse("Deleted"))
+            return HttpResponse("Error")
